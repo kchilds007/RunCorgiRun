d2c79aa667531c3f1cea52a48a8d80e73881301a
Kentaro Childs <kchilds@chapman.edu>
2026-03-03 15:46:56 -0800

    commit day1 unversioned files

---
+++ GameParameters.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+public static class GameParameters
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static float CorgiMoveSpeed = 5f;
+}
---
+++ KeyboardInput.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class KeyboardInput : MonoBehaviour
+{
+    public Corgi Corgi;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+        
+        //if pressed w
+        if (keyboard.wKey.isPressed)
+        {
+            Corgi.Move(Vector2.up);
+        }
+        if (keyboard.sKey.isPressed)
+        {
+            Corgi.Move(Vector2.down);
+
+        }
+        if (keyboard.aKey.isPressed)
+        {
+            Corgi.Move(Vector2.left);
+
+        }
+        if (keyboard.dKey.isPressed)
+        {
+            Corgi.Move(Vector2.right);
+
+        }
+        //move up
+        //etc
+    }
+}
